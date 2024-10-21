3225590246ed27888728ceb14d72e602308265a7
Onyekachi Ibekwe <ibekwe2006@gmail.com>
2024-10-21 12:42:19 -0500

    Adds sideBar component

---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,33 @@
+import { BorderAll, Logout, Splitscreen } from '@mui/icons-material';
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
+import { orange } from '@mui/material/colors';
+
+const Sidebar = () => {
+    return (
+        <div className="flex flex-col py-8 w-20 items-center justify-between border-r">
+            <Logo />
+            <Menu />
+            <Profile />
+        </div>
+    )
+};
+
+const Logo = () => {
+    return <TaskAltIcon fontSize='large' className='text-orange-500' />
+};
+
+const Menu = () => {
+    return (
+        <div className="grid gap-y-8">
+            <BorderAll className="text-orange-500 cursor-pointer"/>
+            <Splitscreen className="text-slate-300 cursor-pointer"/>
+            <Logout className="text-slate-300 cursor-pointer"/>
+        </div>
+    )
+};
+
+const Profile = () => {
+    return <div className="w-8 h-8 bg-orange-500 rounded-md" ></div>
+};
+
+export default Sidebar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Sidebar from "./components/Sidebar";
 
 export default function Home() {
   return (
     <div className="flex h-screen v-screen poppins">
-      This is a project master app
+      <Sidebar />
+      <main className="flex flex-1 h-screen">
+        This is a project master app
+      </main>
     </div>
   );
 }
