c88005e4fe68ac4886c3dddb472ef3ab2c08c945
Onyekachi Ibekwe <ibekwe2006@gmail.com>
2024-10-22 15:31:04 -0500

    Moves from home page to /projects path

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
-import Sidebar from "./components/Sidebar";
-import AllProjectsMain from "./pages/all_projects/components/AllProjectsMain";
+import Sidebar from "../components/Sidebar";
+import AllProjectsMain from "../pages/all_projects/components/AllProjectsMain";
 
-export default function Home() {
+
+export default function ProjectsPage() {
   return (
     <div className="flex h-screen v-screen poppins">
       <Sidebar />
       <main className="flex flex-1 w-full h-screen bg-slate-50">
-        {/* <AllProjectsMain/> */}
+        <AllProjectsMain/>
       </main>
     </div>
   );
