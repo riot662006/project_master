c5cd65bb9d7f68cbdce4aa575eedf1589ff09a9c
Onyekachi Ibekwe <ibekwe2006@gmail.com>
2024-10-31 12:12:23 -0500

    Move Sub-components into main: SelectProjectIconSection

--- app/pages/all_projects/components/SelectProjectIconSection.tsx
+++ app/pages/all_projects/components/SelectProjectIconSection.tsx
@@ -3,8 +3,50 @@ import { allProjectIcons } from "@/app/utils/projectIcons";
 import { useAppContext } from "@/app/utils/AppContext";
 import { Apps } from "@mui/icons-material";
 import { IconData } from "@/app/utils/types";
+import { useWatch } from "react-hook-form";
 
 const SelectProjectIconSection = () => {
+  const { addProjectModalObj } = useAppContext();
+  const { setMode, formData } = addProjectModalObj;
+
+  const icon_id = useWatch({ control: formData.control, name: "icon_id" });
+
+  const Header = () => {
+    return (
+      <div className="flex w-full items-center">
+        <div className="flex items-center gap-2">
+          <div className="flex items-center justify-center rounded-md bg-orange-100 p-2">
+            <Apps sx={{ fontSize: "18px" }} className="text-orange-500" />
+          </div>
+          <span className="text-md font-bold">All Icons</span>
+        </div>
+      </div>
+    );
+  };
+
+  const IconCollection = () => {
+    const selectIcon = (id: number) => {
+      formData.setValue("icon_id", id);
+      setMode("default");
+    };
+
+    return (
+      <ul className="flex h-60 w-full flex-wrap gap-4 overflow-auto overflow-y-auto bg-slate-100 p-4">
+        {allProjectIcons.map((iconData: IconData) => (
+          <button
+            key={`project-icon-${iconData.id}`}
+            onClick={() => selectIcon(iconData.id)}
+          >
+            <ProjectIcon
+              id={iconData.id}
+              outerClassName={`flex item-center justify-center p-2 ${icon_id == iconData.id ? "bg-orange-600 text-white" : "bg-white text-orange-500"} rounded-md cursor-pointer hover:text-white hover:bg-orange-500 `}
+            />
+          </button>
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <>
       <Header />
@@ -18,45 +60,4 @@ const SelectProjectIconSection = () => {
   );
 };
 
-const Header = () => {
-  return (
-    <div className="flex w-full items-center">
-      <div className="flex items-center gap-2">
-        <div className="flex items-center justify-center rounded-md bg-orange-100 p-2">
-          <Apps sx={{ fontSize: "18px" }} className="text-orange-500" />
-        </div>
-        <span className="text-md font-bold">All Icons</span>
-      </div>
-    </div>
-  );
-};
-
-const IconCollection = () => {
-  const { addProjectModalObj } = useAppContext();
-  const { setMode } = addProjectModalObj;
-
-  const { watch, setValue } = addProjectModalObj.formData;
-  const icon_id = watch("icon_id");
-
-  const selectIcon = (id: number) => {
-    setValue("icon_id", id);
-    setMode("default");
-  };
-
-  return (
-    <ul className="flex h-60 w-full flex-wrap gap-4 overflow-auto overflow-y-auto bg-slate-100 p-4">
-      {allProjectIcons.map((iconData: IconData) => (
-        <button
-          key={`project-icon-${iconData.id}`}
-          onClick={() => selectIcon(iconData.id)}
-        >
-          <ProjectIcon
-            id={iconData.id}
-            outerClassName={`flex item-center justify-center p-2 ${icon_id == iconData.id ? "bg-orange-600 text-white" : "bg-white text-orange-500"} rounded-md cursor-pointer hover:text-white hover:bg-orange-500 `}
-          />
-        </button>
-      ))}
-    </ul>
-  );
-};
 export default SelectProjectIconSection;
