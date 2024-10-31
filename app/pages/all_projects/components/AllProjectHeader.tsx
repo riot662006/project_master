6d53dceb06d086dfbcaa248b99d9297bb7445e45
Onyekachi Ibekwe <ibekwe2006@gmail.com>
2024-10-31 12:07:57 -0500

    Move Sub-components into main: AllProjectHeader, ProjectCard

--- app/pages/all_projects/components/AllProjectHeader.tsx
+++ app/pages/all_projects/components/AllProjectHeader.tsx
@@ -5,8 +5,38 @@ import AddProjectModal from "./AddProjectModal";
 import { useAppContext } from "@/app/utils/AppContext";
 
 const AllProjectHeader = () => {
-  const { sidebarObj } = useAppContext();
+  const { sidebarObj, addProjectModalObj } = useAppContext();
+
   const openSidebar = () => sidebarObj.setIsOpen(true);
+  const openAddProjectModal = () => addProjectModalObj.setIsOpen(true);
+
+  const SearchBar = () => {
+    return (
+      <div className="flex items-center">
+        <div className="flex h-10 w-10 items-center justify-center border-b-2 border-orange-500 outline-none">
+          <Search className="text-slate-400" />
+        </div>
+        <div className="h-full w-[67%] border-b-2 border-slate-200">
+          <input
+            placeholder="Search a project..."
+            className="bg-transparent p-2 text-sm outline-none"
+          />
+        </div>
+      </div>
+    );
+  };
+
+  const NewProjectButton = () => {
+    return (
+      <div
+        className="flex h-8 cursor-pointer items-center rounded-md bg-orange-500 pl-2 pr-4 text-xs text-white max-sm:px-2"
+        onClick={openAddProjectModal}
+      >
+        <Add sx={{ fontSize: "22px" }} />
+        <span className="font-medium max-sm:hidden">New Project</span>
+      </div>
+    );
+  };
 
   return (
     <div className="flex items-center justify-between">
@@ -18,37 +48,6 @@ const AllProjectHeader = () => {
         </button>
         <AddProjectModal />
       </div>
-    </div>
-  );
-};
-
-const SearchBar = () => {
-  return (
-    <div className="flex items-center">
-      <div className="flex h-10 w-10 items-center justify-center border-b-2 border-orange-500 outline-none">
-        <Search className="text-slate-400" />
-      </div>
-      <div className="h-full w-[67%] border-b-2 border-slate-200">
-        <input
-          placeholder="Search a project..."
-          className="bg-transparent p-2 text-sm outline-none"
-        />
-      </div>
-    </div>
-  );
-};
-
-const NewProjectButton = () => {
-  const { addProjectModalObj } = useAppContext();
-  const openAddProjectModal = () => addProjectModalObj.setIsOpen(true);
-
-  return (
-    <div
-      className="flex h-8 cursor-pointer items-center rounded-md bg-orange-500 pl-2 pr-4 text-xs text-white max-sm:px-2"
-      onClick={openAddProjectModal}
-    >
-      <Add sx={{ fontSize: "22px" }} />
-      <span className="font-medium max-sm:hidden">New Project</span>
     </div>
   );
 };
--- app/pages/all_projects/components/ProjectCard.tsx
+++ app/pages/all_projects/components/ProjectCard.tsx
@@ -1,87 +1,85 @@
 import ProjectIcon from "@/app/components/ProjectIcon";
-import { IconName } from "@/app/utils/projectIcons";
 import { Project } from "@/app/utils/types";
-import { MoreVert, Splitscreen } from "@mui/icons-material";
+import { MoreVert } from "@mui/icons-material";
 import Circle from "@mui/icons-material/Circle";
 
 const ProjectCard = ({ project }: { project: Project }) => {
   const { title, icon } = project;
 
-  return (
-    <div className="flex w-80 flex-col gap-8 bg-white p-7 px-10 max-sm:w-full">
-      <ProjectCardHeader title={title} icon={icon} />
-      <ProjectCardBody />
-      <ProjectCardFooter />
-    </div>
-  );
-};
-
-const ProjectCardHeader = ({
-  title,
-  icon,
-}: {
-  title: string;
-  icon: IconName;
-}) => {
-  return (
-    <div className="flex items-center justify-between">
-      {/* Title and Icon */}
-      <div className="flex items-center gap-3">
-        {/* Icon */}
-        <ProjectIcon
-          name={icon}
-          outerClassName="flex aspect-square w-10 items-center justify-center rounded-md bg-orange-500"
-          innerClassName="text-white"
-          sx={{ fontSize: "20px" }}
-        />
-        {/* Title */}
-        <div className="flex flex-col">
-          <span className="text-m font-bold">{title}</span>
-          <span className="text-[12px] text-slate-400">2 days ago</span>
+  const ProjectCardHeader = () => {
+    return (
+      <div className="flex items-center justify-between">
+        {/* Title and Icon */}
+        <div className="flex items-center gap-3">
+          {/* Icon */}
+          <ProjectIcon
+            name={icon}
+            outerClassName="flex aspect-square w-10 items-center justify-center rounded-md bg-orange-500"
+            innerClassName="text-white"
+            sx={{ fontSize: "20px" }}
+          />
+          {/* Title */}
+          <div className="flex flex-col">
+            <span className="text-m font-bold">{title}</span>
+            <span className="text-[12px] text-slate-400">2 days ago</span>
+          </div>
+        </div>
+        {/* Options */}
+        <div>
+          <MoreVert
+            fontSize="small"
+            className="cursor-pointer text-slate-400"
+          />
         </div>
       </div>
-      {/* Options */}
-      <div>
-        <MoreVert fontSize="small" className="cursor-pointer text-slate-400" />
+    );
+  };
+
+  const ProjectCardBody = () => {
+    return (
+      <ul className="ml-2 flex flex-col gap-4 text-sm text-slate-400">
+        <ProjectCardBodyItem />
+        <ProjectCardBodyItem />
+      </ul>
+    );
+  };
+
+  const ProjectCardBodyItem = () => {
+    return (
+      <li className="flex items-center gap-2">
+        <Circle sx={{ fontSize: "8px" }} />
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+        </span>
+      </li>
+    );
+  };
+
+  const ProjectCardFooter = () => {
+    return (
+      <div className="flex flex-col gap-4">
+        <ProjectProgressBar />
+        <div className="flex items-center justify-between text-xs">
+          <span className="text-slate-400">On Progress</span>
+          <span className="font-semibold">33%</span>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-const ProjectCardBody = () => {
-  return (
-    <ul className="ml-2 flex flex-col gap-4 text-sm text-slate-400">
-      <ProjectCardBodyItem />
-      <ProjectCardBodyItem />
-    </ul>
-  );
-};
-
-const ProjectCardBodyItem = () => {
-  return (
-    <li className="flex items-center gap-2">
-      <Circle sx={{ fontSize: "8px" }} />
-      <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed</span>
-    </li>
-  );
-};
-
-const ProjectCardFooter = () => {
-  return (
-    <div className="flex flex-col gap-4">
-      <ProjectProgressBar />
-      <div className="flex items-center justify-between text-xs">
-        <span className="text-slate-400">On Progress</span>
-        <span className="font-semibold">33%</span>
+  const ProjectProgressBar = () => {
+    return (
+      <div className="h-2 w-full rounded-lg bg-slate-100">
+        <div className="h-2 w-[33%] rounded-lg bg-orange-500" />
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-const ProjectProgressBar = () => {
   return (
-    <div className="h-2 w-full rounded-lg bg-slate-100">
-      <div className="h-2 w-[33%] rounded-lg bg-orange-500" />
+    <div className="flex w-80 flex-col gap-8 bg-white p-7 px-10 max-sm:w-full">
+      <ProjectCardHeader />
+      <ProjectCardBody />
+      <ProjectCardFooter />
     </div>
   );
 };
