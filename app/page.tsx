008b3850470c6f8647904aa44d94bb065dd5b6be
Onyekachi Ibekwe <ibekwe2006@gmail.com>
2024-11-05 11:39:49 -0600

    Add redirection from home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+"use client"; // Enable client-side rendering
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/projects");
+    }, 2000);
+
+    return () => clearTimeout(timer); // Cleanup timer if the component unmounts
+  }, [router]);
+
   return (
     <main className="flex h-screen w-full flex-1 bg-slate-50">
-      <p className="text-3xl font-extrabold">Nothing for now</p>
+      <p className="text-3xl font-bold">
+        Nothing here for now... Redirecting to projects page
+      </p>
     </main>
   );
 }
