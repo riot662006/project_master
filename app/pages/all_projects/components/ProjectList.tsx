9fa5c2fda556515a47a2ab6413d4944465969f2e
Onyekachi Ibekwe <ibekwe2006@gmail.com>
2024-11-05 11:45:35 -0600

    Logic fix No project placeholder

--- app/pages/all_projects/components/ProjectList.tsx
+++ app/pages/all_projects/components/ProjectList.tsx
@@ -8,14 +8,16 @@ import { ListAlt } from "@mui/icons-material";
 const ProjectList = () => {
   const { allProjects } = useAppContext();
 
-  if (allProjects.length > 1) {
+  if (allProjects.length == 0) {
     return (
       <div className="flex h-full w-full flex-col items-center justify-center gap-4 text-slate-300">
         <div>
-          <ListAlt sx={{ fontSize: "120px" }}/>
+          <ListAlt sx={{ fontSize: "120px" }} />
         </div>
         <div className="flex flex-col items-center gap-4">
-          <h2 className="font-semibold text-slate-400">No Projects Created Yet...</h2>
+          <h2 className="font-semibold text-slate-400">
+            No Projects Created Yet...
+          </h2>
           <div className="flex flex-col items-center text-sm">
             <p>It looks like you haven&apos;t started any projects yet.</p>
             <p>Create a new project to begin managing your tasks</p>
