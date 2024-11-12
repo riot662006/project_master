1a6e8895c80e9f780fc6915b5437a3dd61329b40
Onyekachi Ibekwe <ibekwe2006@gmail.com>
2024-11-11 19:17:46 -0600

    Completed projects logic (title and numbers)

--- app/projects/components/Sections/CompletedProjectsSection.tsx
+++ app/projects/components/Sections/CompletedProjectsSection.tsx
@@ -1,48 +1,74 @@
 "use client";
 
 import ProjectProgressCircle from "@/components/CircularProgressBar";
+import { useAppSelector } from "@/hooks/storeHooks";
+import { Project } from "@/utils/types";
 import Splitscreen from "@mui/icons-material/Splitscreen";
+import { Fragment } from "react";
 
 const CompletedProjectsSection = () => {
+  const projects = useAppSelector((state) => state.projects.projectsList);
+
+  const completedProjects = projects.filter((project) =>
+    project.tasks.every((task) => task.status == "completed"),
+  );
+  const completedPercentage = Math.floor(
+    (completedProjects.length / projects.length) * 100,
+  );
+  const totalTasksDone = projects.reduce(
+    (acc: number, project) =>
+      acc + project.tasks.filter((task) => task.status == "completed").length,
+    0,
+  );
+
   return (
-    <div className="flex h-full w-full flex-col items-center gap-6 rounded-2xl bg-white p-8">
+    <div className="flex h-full w-full flex-col items-center gap-20 rounded-2xl bg-white p-8">
       <h2 className="font-bold">Projects Completed</h2>
-      <ProjectProgressCircle percentage={59} radius={80} strokeWidth={12} />
+      <ProjectProgressCircle
+        percentage={completedPercentage}
+        radius={80}
+        strokeWidth={12}
+      />
 
       {/* Labels */}
       <div className="flex flex-col items-center gap-1">
-        <p className="text-sm font-semibold">3 Completed</p>
-        <p className="text-[10px] text-slate-400">20 Tasks done</p>
+        <p className="text-sm font-semibold">
+          {completedProjects.length} Completed
+        </p>
+        <p className="text-[10px] text-slate-400">
+          {totalTasksDone} Tasks done
+        </p>
       </div>
 
-      <ProjectsList />
+      <ProjectsList completed={completedProjects} />
     </div>
   );
 };
 
-const ProjectsList = () => {
+const ProjectsList = ({ completed }: { completed: Project[] }) => {
   return (
     <ul className="flex w-full flex-col items-center gap-5 overflow-auto">
-      <SingleProject />
-      <hr className="w-[80%]" />
-      <SingleProject />
-      <hr className="w-[80%]" />
-      <SingleProject />
-      <hr className="w-[80%]" />
-      <SingleProject />
+      {completed.map((project, index) => (
+        <Fragment key={index}>
+          <SingleProject project={project} />
+          {index < completed.length - 1 && <hr className="w-[80%]" />}
+        </Fragment>
+      ))}
     </ul>
   );
 };
 
-const SingleProject = () => {
+const SingleProject = ({ project }: { project: Project }) => {
   return (
     <li className="flex w-full gap-2">
       <div className="flex aspect-square w-8 items-center justify-center rounded-md bg-sky-500">
         <Splitscreen sx={{ fontSize: "18px" }} className="text-white" />
       </div>
       <div className="flex flex-col">
-        <p className="text-xs font-semibold">Project 1</p>
-        <p className="text-[10px] text-slate-400">3 Tasks</p>
+        <p className="text-xs font-semibold">{project.title}</p>
+        <p className="text-[10px] text-slate-400">
+          {project.tasks.length} Tasks
+        </p>
       </div>
     </li>
   );
