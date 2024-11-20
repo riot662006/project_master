2af2d438b6b4e7fdd1bf92f48c7baf1384617509
Onyekachi Ibekwe <ibekwe2006@gmail.com>
2024-11-20 12:59:28 -0600

    Move search functionality to modal

--- app/tasks/components/Dropdowns/TaskProjectSelector.tsx
+++ app/tasks/components/Dropdowns/TaskProjectSelector.tsx
@@ -2,6 +2,7 @@ import { useAppSelector } from "@/hooks/storeHooks";
 import { useDetectOutsideClick } from "@/hooks/useDetectOutsideClick";
 import { selectProjects } from "@/store/Selectors";
 import { IAddTaskFormInput } from "@/utils/types";
+import { KeyboardArrowDown } from "@mui/icons-material";
 import React, { useEffect, useRef, useState } from "react";
 import { useController, UseControllerProps } from "react-hook-form";
 
@@ -10,14 +11,6 @@ const TaskProjectSelector = (
 ) => {
   const { field, fieldState } = useController(props);
   const projects = useAppSelector(selectProjects("name"));
-
-  const projectNamesToId = projects.reduce(
-    (acc, project) => {
-      acc[project.title] = project.id;
-      return acc;
-    },
-    {} as { [key: string]: string },
-  );
 
   const menuRef = useRef<HTMLDivElement>(null);
   const [isMenuActive, toggleMenu, closeMenu] = useDetectOutsideClick(
@@ -30,22 +23,14 @@ const TaskProjectSelector = (
   const onSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.target.value);
   };
-  const onSearchInputFocus = () => {
-    toggleMenu();
-  };
   const onSearchInputKeydown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key == "Enter") {
       e.preventDefault();
-      const projectId = projectNamesToId[e.currentTarget.value];
-      if (projectId) {
-        field.onChange(projectId);
-      }
     }
   };
-
   const onProjectClicked = (value: string) => {
     field.onChange(value);
-    setSearchValue(projects.find((project) => project.id == value)?.title ?? "");
+    setSearchValue("");
     closeMenu();
   };
 
@@ -53,37 +38,51 @@ const TaskProjectSelector = (
     project.title.toLowerCase().includes(searchValue.toLowerCase()),
   );
 
+  const curSelectedProject = projects.find(
+    (project) => project.id === field.value,
+  );
+
   return (
     <div className="relative flex w-full flex-col text-sm">
       <div className="flex w-full flex-col" ref={menuRef}>
         <div className="flex w-full rounded-sm border p-2">
-          <input
-            className="flex flex-grow items-center gap-2 truncate text-ellipsis whitespace-nowrap outline-none"
-            placeholder="Select Project..."
-            onChange={onSearchInputChange}
-            onFocus={onSearchInputFocus}
-            value={searchValue}
-            onKeyDown={onSearchInputKeydown}
-          />
+          <span
+            className={`flex flex-grow items-center gap-2 truncate text-ellipsis whitespace-nowrap ${!curSelectedProject && "text-slate-400"}`}
+            onClick={toggleMenu}
+          >
+            {curSelectedProject?.title ?? "Select Project..."}
+          </span>
+          <KeyboardArrowDown />
         </div>
         <div
-          className={`absolute top-14 z-40 flex w-full flex-col ${isMenuActive ? "block" : "hidden"} rounded-md border-gray-200 bg-white px-2 py-4 shadow-md`}
+          className={`absolute top-14 z-40 flex w-full flex-col ${isMenuActive ? "block" : "hidden"} gap-2 rounded-md border-gray-200 bg-white px-2 py-4 shadow-md`}
         >
-          {searchedProjects.length ? (
-            searchedProjects.map((project) => (
-              <span
-                key={project.id}
-                className={`cursor-default rounded-sm p-2 ${project.id == field.value ? "border border-sky-300 bg-sky-200" : "hover:bg-sky-100"}`}
-                onClick={() => onProjectClicked(project.id)}
-              >
-                {project.title}
+          <div className="flex w-full">
+            <input
+              placeholder="Search..."
+              value={searchValue}
+              onChange={onSearchInputChange}
+              onKeyDown={onSearchInputKeydown}
+              className="w-full rounded-sm p-2 outline outline-2 outline-slate-200 focus:outline-sky-400"
+            />
+          </div>
+          <div className="flex max-h-36 flex-col overflow-y-auto">
+            {searchedProjects.length ? (
+              searchedProjects.map((project) => (
+                <span
+                  key={project.id}
+                  className={`cursor-default rounded-sm p-2 ${project.id == field.value ? "border border-sky-300 bg-sky-200" : "hover:bg-sky-100"}`}
+                  onClick={() => onProjectClicked(project.id)}
+                >
+                  {project.title}
+                </span>
+              ))
+            ) : (
+              <span className="text-center text-xs italic text-slate-300">
+                No projects found
               </span>
-            ))
-          ) : (
-            <span className="text-center text-xs italic text-slate-300">
-              No projects found
-            </span>
-          )}
+            )}
+          </div>
         </div>
       </div>
       {fieldState.error && (
