cdc390345d0ffb3b549c901ea42c22f6f3b47a9b
Onyekachi Ibekwe <72505429+riot662006@users.noreply.github.com>
2024-11-25 18:45:43 +0000

    Differentiates loading states in taskSection

--- app/tasks/components/Sections/TasksSection.tsx
+++ app/tasks/components/Sections/TasksSection.tsx
@@ -6,14 +6,15 @@ import TaskList from "../Lists/TaskList";
 import { CircularProgress } from "@mui/material";
 import AddTaskModal from "../Modals/AddTaskModal";
 import ConfirmDeleteModal from "@/components/Modals/ConfirmProjectDeleteModal";
+import LoadingContainer from "@/components/Containers/LoadingContainer";
 
 const TasksSection = () => {
-  const isLoading = useAppSelector((state) => state.projects.isLoading);
+  const IsFetching = useAppSelector((state) => state.projects.isFetching);
 
   return (
-    <div className="flex h-full w-full px-8 py-10">
+    <LoadingContainer className="flex h-full w-full px-8 py-10">
       <div className="flex h-full w-full flex-col px-8 py-10 max-lg:px-0">
-        {isLoading ? (
+        {IsFetching ? (
           <div className="flex h-full w-full items-center justify-center">
             <CircularProgress size="5rem" sx={{ color: "skyblue" }} />
           </div>
@@ -26,7 +27,7 @@ const TasksSection = () => {
       </div>
       <AddTaskModal />
       <ConfirmDeleteModal />
-    </div>
+    </LoadingContainer>
   );
 };
 
