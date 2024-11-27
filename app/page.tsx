26e255976a3a7e331f1b2d4df7cec11b57c17c46
Onyekachi Ibekwe <72505429+riot662006@users.noreply.github.com>
2024-11-27 23:54:51 +0000

    Makes responsive

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,25 @@
 import Logo from "@/components/Icons/Logo";
 import Image from "next/image";
 import KeyFeaturesList from "./home/Lists/KeyFeaturesList";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 export default function Home() {
+  const isMaxSm = useMediaQuery("not all and (min-width: 640px)");
+
   return (
     <main className="flex w-full flex-col bg-white">
-      <nav className="flex w-full justify-between p-8">
-        <Logo showName />
+      <nav className="flex w-full items-center justify-between p-8">
+        <Logo showName={!isMaxSm} />
         <button className="flex items-center rounded-md bg-sky-500 p-2 px-4 text-sm text-white">
           Get started
         </button>
       </nav>
-      <section className="flex flex-col items-center gap-4 p-20">
-        <h1 className="text-xl font-bold">
+      <section className="flex flex-col items-center gap-4 p-20 max-sm:px-8">
+        <h1 className="text-center text-xl font-bold">
           Manage Your Projects and Tasks{" "}
           <span className="text-sky-500">Effortlessly!</span>
         </h1>
-        <p className="w-96 text-center text-sm font-medium text-slate-500">
+        <p className="w-96 text-center text-sm font-medium text-slate-500 max-sm:w-full">
           Supercharge your workflow with our intuitive platform! Organize tasks,
           track progress, and hit deadlines—effortlessly. Stay on top and
           achieve more, faster!
