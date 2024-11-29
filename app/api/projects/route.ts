e69ea5c435cee70f01bfb484674b9c24949519c4
Onyekachi Ibekwe <72505429+riot662006@users.noreply.github.com>
2024-11-29 21:36:04 +0000

    Creates prisma projects api call

---
+++ app/api/projects/route.ts
@@ -0,0 +1,38 @@
+import { getAuth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export const GET = async (req: NextRequest) => {
+  const { userId } = getAuth(req);
+
+  if (!userId) {
+    return NextResponse.json(
+      { error: "Unauthorized: User not authenticated" },
+      { status: 401 },
+    );
+  }
+
+  const url = new URL(req.url);
+  const requestedUserId = url.searchParams.get("userId");
+
+  if (!requestedUserId || requestedUserId !== userId) {
+    return NextResponse.json(
+      { error: "Forbidden: Access to this resource is denied" },
+      { status: 403 },
+    );
+  }
+
+  try {
+    const projects = await prisma.project.findMany({
+      where: { clerkUserId: userId },
+      include: { tasks: true },
+    });
+    return NextResponse.json(projects)
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Failed to fetch projects" },
+      { status: 500 },
+    );
+  }
+};
