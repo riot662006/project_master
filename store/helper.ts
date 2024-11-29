af0fe82aaf6c7c8927ce78fc8c7cc46bcdb5c7cf
Onyekachi Ibekwe <72505429+riot662006@users.noreply.github.com>
2024-11-29 22:42:11 +0000

    Adds asyncThunk helper function

---
+++ store/helper.ts
@@ -0,0 +1,31 @@
+import showToast from "react-hot-toast";
+
+export async function apiRequest<T>(
+  url: string,
+  method: "GET" | "POST" | "PUT" | "DELETE",
+  body?: Record<string, unknown>,
+  toast?: { success?: string; error?: string },
+): Promise<T> {
+  try {
+    const response = await fetch(url, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: body ? JSON.stringify(body) : undefined,
+    });
+
+    if (!response.ok) {
+      throw new Error(toast?.success || "An error occurred");
+    }
+
+    const data: T = await response.json();
+    if (toast?.success !== undefined)
+      showToast.success(toast?.success || "Request successful");
+    return data;
+  } catch (error: any) {
+    if (toast?.error !== undefined)
+      showToast.error(toast?.error || "An error occurred");
+    throw error;
+  }
+}
