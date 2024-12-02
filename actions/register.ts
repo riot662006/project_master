77071adcf416aa486070877ad8058e7a40eb0518
Onyekachi Ibekwe <ibekwe2006@gmail.com>
2024-12-02 14:32:28 -0600

    Starts adding registration logic

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,12 @@
 "use server";
 
+import bcrypt from "bcrypt";
+
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
+
+import prisma from "@/lib/prisma";
+import { getUserByEmail } from "@/lib/api/helper";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -10,5 +15,22 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await getUserByEmail(email);
+
+  if (user) return { error: "Email already in use" };
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification email
+
+  return { success: "User created!" };
 };
--- lib/api/helper.ts
+++ lib/api/helper.ts
@@ -1,62 +1,29 @@
-import { getAuth } from "@clerk/nextjs/server";
-import { ApiError, ForbiddenError, UnauthorizedError } from "./errors";
-import { NextRequest, NextResponse } from "next/server";
-
 import prisma from "../prisma";
 
-export const getAuthenticatedUser = (req: NextRequest): { userId: string } => {
-  const { userId } = getAuth(req);
-
-  if (!userId) {
-    throw new UnauthorizedError();
+export const getUserByEmail = async (email: string) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    return user;
+  } catch (error) {
+    console.error(error);
+    return null;
   }
-
-  return { userId };
 };
 
-export const handleApiError = (
-  error: unknown,
-  fallbackMsg: string = "An unexpected error occurred",
-): NextResponse<{
-  error: string;
-}> => {
-  if (error instanceof ApiError) {
-    return NextResponse.json(
-      { error: error.message },
-      { status: error.status },
-    );
+export const getUserById = async (id: string) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+    return user;
+  } catch (error) {
+    console.error(error);
+    return null;
   }
-  console.error("Unexpected error: ", error);
-  return NextResponse.json({ error: fallbackMsg }, { status: 500 });
 };
-
-export const verifyProjectOwnership = async (
-  projectId: string,
-  userId: string,
-) => {
-  const project = await prisma.project.findUnique({ where: { id: projectId } });
-
-  if (!project || project.clerkUserId !== userId) {
-    throw new ForbiddenError();
-  }
-  return project;
-};
-
-export async function verifyTaskOwnership(
-  taskId: string,
-  projectOrUserId: string,
-) {
-  const task = await prisma.task.findUnique({
-    where: { id: taskId },
-    include: { project: true },
-  });
-
-  if (
-    !task ||
-    (task.projectId !== projectOrUserId &&
-      task.project.clerkUserId !== projectOrUserId)
-  ) {
-    throw new ForbiddenError();
-  }
-  return task;
-}
