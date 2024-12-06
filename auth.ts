57419d5bd1674bb69fffe5830ba60541ecf7f502
Onyekachi Ibekwe <ibekwe2006@gmail.com>
2024-12-05 20:02:38 -0600

    Adds callbacks to auth

--- auth.ts
+++ auth.ts
@@ -6,6 +6,27 @@ import authConfig from "./auth.config";
 const prisma = new PrismaClient();
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    // TODO: Add when made email verification
+    // async signIn({ user }) {
+    //   if (!user.id) return false;
+
+    //   const existingUser = await getUserById(user.id);
+
+    //   if (!existingUser || !existingUser.emailVerified) {
+    //     return false;
+    //   }
+
+    //   return true;
+    // },
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      return session;
+    },
+  },
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   ...authConfig,
