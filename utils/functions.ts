1531bb0df28817c1fb04381aa7d6efc8c1c0b42e
Onyekachi Ibekwe <ibekwe2006@gmail.com>
2024-12-11 16:23:52 -0600

    Updates `UserButton`

--- utils/functions.ts
+++ utils/functions.ts
@@ -28,3 +28,21 @@ export const calculateProgressPercentage = (
 ): number => {
   return totalTasks > 0 ? Math.round((completedTasks / totalTasks) * 100) : 0;
 };
+
+export const stringHashedColor = (string: string): string => {
+  let hash = 0;
+  let i;
+
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = "#";
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+
+  return color;
+};
