97febfe7cb908f4b129a5d950312d3b557aa1e6f
Onyekachi Ibekwe <ibekwe2006@gmail.com>
2024-12-11 12:56:50 -0600

    Adds suspense to auth layout

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,7 +1,14 @@
+import { CircularProgress } from "@mui/material";
+import { Suspense } from "react";
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center bg-slate-50">
-      {children}
+      <Suspense
+        fallback={<CircularProgress size="4rem" sx={{ color: "skyblue" }} />}
+      >
+        {children}
+      </Suspense>
     </div>
   );
 };
